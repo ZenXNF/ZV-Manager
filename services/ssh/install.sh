891c169543999b4a013cb1d6796b2ca9b63ee190
ZenXNF <zaenzenn2008@gmail.com>
2026-03-01 06:57:59 +0700

    Add files via upload

--- services/ssh/install.sh
+++ services/ssh/install.sh
@@ -40,21 +40,19 @@ install_ssh() {
     # Cek apakah Banner sudah ada di config
     grep -q "^Banner" "$sshd_config" || echo "Banner /etc/issue.net" >> "$sshd_config"
 
-    # Isi banner dengan warna ANSI
-    cat > /etc/issue.net <<'BANNEREOF'
-\e[1;36m╔══════════════════════════════════════════════════╗\e[0m
-\e[1;36m║\e[0m        \e[1;33m--- WELCOME TO ZV-MANAGER ---\e[0m           \e[1;36m║\e[0m
-\e[1;36m╚══════════════════════════════════════════════════╝\e[0m
-
-\e[1;31m        ! TERM OF SERVICE !\e[0m
-\e[1;37m        NO SPAM\e[0m
-\e[1;37m        NO DDOS\e[0m
-\e[1;37m        NO HACKING & CARDING\e[0m
-\e[1;31m        NO TORRENT !!\e[0m
-\e[1;31m        NO MULTI LOGIN !!\e[0m
-
-\e[1;36m══════════════════════════════════════════════════\e[0m
-BANNEREOF
+    # Isi banner dengan warna ANSI (pakai printf agar \e dirender)
+    printf '\e[1;36m╔══════════════════════════════════════════════════╗\e[0m\n' > /etc/issue.net
+    printf '\e[1;36m║\e[0m        \e[1;33m--- WELCOME TO ZV-MANAGER ---\e[0m           \e[1;36m║\e[0m\n' >> /etc/issue.net
+    printf '\e[1;36m╚══════════════════════════════════════════════════╝\e[0m\n' >> /etc/issue.net
+    printf '\n' >> /etc/issue.net
+    printf '\e[1;31m        ! TERM OF SERVICE !\e[0m\n' >> /etc/issue.net
+    printf '\e[1;37m        NO SPAM\e[0m\n' >> /etc/issue.net
+    printf '\e[1;37m        NO DDOS\e[0m\n' >> /etc/issue.net
+    printf '\e[1;37m        NO HACKING & CARDING\e[0m\n' >> /etc/issue.net
+    printf '\e[1;31m        NO TORRENT !!\e[0m\n' >> /etc/issue.net
+    printf '\e[1;31m        NO MULTI LOGIN !!\e[0m\n' >> /etc/issue.net
+    printf '\n' >> /etc/issue.net
+    printf '\e[1;36m══════════════════════════════════════════════════\e[0m\n' >> /etc/issue.net
 
     # Setup /etc/shells
     grep -qx '/bin/false' /etc/shells || echo '/bin/false' >> /etc/shells
